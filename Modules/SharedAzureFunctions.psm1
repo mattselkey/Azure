27b2e7617e9e73d2a9267036d0424b54bf41b154
mattselkey <matt@mattselkey.com>
2019-10-27 19:05:13 +0100

    moved reusable functions to module

---
+++ Modules/SharedAzureFunctions.psm1
@@ -0,0 +1,49 @@
+<#
+.SYNOPSIS
+    Short description
+.DESCRIPTION
+    Long description
+.EXAMPLE
+    PS C:\> <example usage>
+    Explanation of what the example does
+.INPUTS
+    Inputs (if any)
+.OUTPUTS
+    Output (if any)
+.NOTES
+    General notes
+#>
+
+
+function Import-AZprofile{
+    Write-Information -MessageData "Logging into Azure using saved profile"
+    try{
+        $AzContent = Import-AzContext -Path $AZContextPath -ErrorAction stop
+        #Import-AzContext -Path $AZContextPath 
+        return $AzContent
+    }
+    catch{
+        Write-Error -Message "Error loading profile: $($_)"
+        Pause
+        exit
+    }
+}
+
+function Import-AllAzureModules{
+
+    Write-Information -MessageData "Checking if Azure Modules are loaded. Loading if needed" 
+    $AZModules = Get-Module | Where-Object {($_.Name -like "*Az.*")} 
+    #$AZResourceManagerModules = Get-Module | Where-Object {($_.Name -like "AzureRM*")}
+    
+    if(($null -eq $AZModules)){
+        Write-Information -MessageData "Azure Modules are not loaded, loading Modules" 
+        Find-Module -Name Az | Install-Module -AllowClobber -Force
+    
+        }
+        else{
+        Write-Information -MessageData "Azure Modules are loaded."
+        }
+
+}
+
+Export-ModuleMember -Function *
