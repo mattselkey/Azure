4cd5bb07501b719dbc62f781808248a0b1349bb6
Matthew Key <matt@mattselkey.com>
2020-09-01 13:31:16 +0200

    fixes

--- QuickDeploy/ARMTemplates/LBwithVMs/deploy.ps1
+++ QuickDeploy/ARMTemplates/LBwithVMs/deploy.ps1
@@ -1,12 +1,25 @@
+#Connect-AzAccount
+
 $projectName = Read-Host -Prompt "Enter a project name with 12 or less letters or numbers that is used to generate Azure resource names"
-$location = Read-Host -Prompt "Enter the location (i.e. centralus)"
+$location = Read-Host -Prompt "Enter the location (i.e. northeurope)"
 $adminUserName = Read-Host -Prompt "Enter the virtual machine administrator account name"
 $adminPassword = Read-Host -Prompt "Enter the virtual machine administrator password" -AsSecureString
 
+$PSScriptRoot
+
 $resourceGroupName = "${projectName}rg"
-$templateUri = "https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-load-balancer-standard-create/azuredeploy.json"
+$templateUri = "$($PSScriptRoot)\template.json"    
+
+$additionalParameters = @{
+
+    adminUsername = $adminUserName
+    projectName = $projectName
+    adminPassword = $adminPassword
+    location = $location
+}
+
 
 New-AzResourceGroup -Name $resourceGroupName -Location $location
-New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -projectName $projectName -location $location -adminUsername $adminUsername -adminPassword $adminPassword
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri @additionalParameters 
 
 Write-Host "Press [ENTER] to continue."
